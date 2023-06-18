0d9aacc633acc0542d0d8816053ec15f0dc87087
Juliano <juliano.co.lopes@gmail.com>
2023-06-18 10:13:24 -0300

    create the script for python venv and updates in older ones

--- Readme.md
+++ Readme.md
@@ -1,6 +1,7 @@
-# Virtual Environment Choice for Anaconda
+# Virtual Environment Choice
 
-This script allows the user make a choice between virtual environments for anaconda by the time they start a terminal window<br><br>
+
+<br>This script ([Anaconda](./choice-env-anaconda.sh) | [venv](./choice-env-venv.sh)) allows the user make a choice between virtual environments for anaconda or python venv by the time they start a terminal window<br><br>
 
 For this, the user must include this script to the end of <code>.bashrc</code> file in your Ubuntu (the one operation system that has been tested).<br><br>
 
@@ -12,10 +13,15 @@ Following the steps:
 * <code>CTRL + X</code> to be asked to save the file
 * Click <code>Y</code> to confirm
 
-Anaconda must be installed too.<br><br>
+Then, in your terminal window, type the command:
+* <code>source .bashrc</code> to update or close and open a terminal window again.
+
+In case you use Anaconda, it must be installed too.<br><br>
 
 The Script will read the environments that are defined and will include a new one in case it finds one.<br><br>
 
-Then it show the options to user, who will make their choice.<br><br>
+Next, it show the options to user, who will make their choice.<br><br>
 
-OBS: In case the terminal is an vscode terminal integrated, the script will select the interpreter anaconda python that user defined as default.
+OBS: In case the terminal is an vscode terminal integrated, the script will select the interpreter python that user defined as default or will deliver the default prompt if not.
+
+<hr>
--- choice-env-venv.sh
+++ choice-env-venv.sh
@@ -1,26 +1,44 @@
 # Verificar se é o terminal integrado do VSCODE
 if [[ "$TERM_PROGRAM" == "vscode" ]]; then
     # O usuário deve trocar <interpretador> por seu interpretador anaconda default do vscode
-    conda deactivate && conda activate <interpretador>
+    echo "vscode"
+    . <nome-ambiente>/bin/activate
 else
-    
+    # Busca por ambientes virtuais a partir da home
+    listenv=()
+    output=$(find ~ | grep bin/activate$)
+    IFS=$'\n' read -rd '' -a listenv <<< "$output"
+    unset output
+
+    envs=()
+    fullenvs=()
+
+    for env in "${listenv[@]}"; do
+        IFS='/'
+        read -ra arr <<< $env
+        path=""
+        for ((i=0;i<${#arr[@]}-2;i++)); do
+            if [[ $i -lt ${#arr[@]}-2 ]]; then
+                path+="${arr[i]}"
+                if [[ $i -ne ${#arr[@]}-3 ]]; then
+                    path+="/"
+                fi
+            fi
+            if [[ $i -eq ${#arr[@]}-3 ]]; then
+                envs+=(${arr[i]})
+            fi
+        done
+        fullenvs+=("$path")
+    done
+    unset path listenv
 
     # Obter a lista de ambientes virtuais
     echo "Selecione o ambiente virtual: " $'\n'
-
-    # O usuário deve trocar <user> pelo nome do seu usuário
-    environments=($(ls /home/<user>/anaconda3/envs)) 
-
-    options=("base")  # Adiciona a opção padrão
-
-    # Adicionar as opções dos ambientes virtuais
-    for env in "${environments[@]}"; do
-        options+=("$env")
-    done
+    
 
     # Imprime a lista de ambientes virtuais para que o usuário faça sua escolha
-    for ((i=1; i<=${#options[@]}; i++)); do
-        echo "$i ${options[i - 1]}"
+    for ((i=1; i<=${#envs[@]}; i++)); do
+        echo "$i ${envs[i - 1]}"
     done
 
     # Função que capturará o input de escolha do usuário
@@ -28,12 +46,12 @@ else
         read -r choice
         if [[ -z "$choice" ]]; then 
             # Condição if para capturar um Enter
-            opt="base"
-        elif [[ ! "$choice" =~ ^[0-9]+$ || "$choice" -lt 1 || "$choice" -gt ${#options[@]} ]]; then
+            opt="none"
+        elif [[ ! "$choice" =~ ^[0-9]+$ || "$choice" -lt 1 || "$choice" -gt ${#envs[@]} ]]; then
             # Condição para identificar opções inválidas
             echo "Opção Inválida"
             tela="Escolha números entre "
-            for ((i=1; i<=${#options[@]}; i++)); do
+            for ((i=1; i<=${#envs[@]}; i++)); do
                 tela+="$i, "
             done
             tela+="ou tecle Enter para opção padrão."
@@ -41,20 +59,22 @@ else
             select_option
         else
             # Condição para armazenar a escolha feita
-            opt="${options[choice-1]}"
+            opt="${fullenvs[choice-1]}"
         fi
     }
 
     # Chamando a função de entrada do usuário
     select_option
 
+    unset envs fullenvs
+
     # A opção que será selecionada de acordo com a escolha do usuário
     case $opt in
-        "base")
-            conda activate base  # Substitua pelo caminho do seu ambiente virtual 
+        "none")
+            echo ''
             ;;
         *)
-            conda deactivate && conda activate $opt  # Substitua pelo caminho do seu ambiente virtual
+            . "$opt"/bin/activate  # Substitua pelo caminho do seu ambiente virtual
             ;;
         *) echo "Opção inválida";;
     esac
